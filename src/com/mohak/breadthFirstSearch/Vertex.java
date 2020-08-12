d374a1ae928cbf1d7cfa8da67c806d103e68976e
mohak92 <tamhane.mohak@gmail.com>
2020-08-11 22:49:30 -0400

    bfs example vertex

---
+++ src/com/mohak/breadthFirstSearch/Vertex.java
@@ -0,0 +1,38 @@
+package com.mohak.breadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+
+	private String name;
+	private boolean visited;
+	private List<Vertex> adjacencyList;
+	
+	public Vertex(String name) {
+		this.name =  name;
+		adjacencyList = new ArrayList<>();
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Vertex> getAdjacencyList() {
+		return adjacencyList;
+	}
+
+	public void addNeighbor(Vertex vertex) {
+		this.adjacencyList.add(vertex);
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+}
